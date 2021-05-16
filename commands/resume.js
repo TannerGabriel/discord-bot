25a125d0b63eb286b576176602050e121e8c193c
Benoit Gloria <bgloria@Nexcloud-2.0>
2021-05-16 14:14:38 +0200

    fix typo

--- commands/resume.js
+++ commands/resume.js
@@ -5,7 +5,7 @@ module.exports = {
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!message.member.voice.channel) return message.channel.send('You have to be in a voice channel to resume the music!');
 		if (!serverQueue) return message.channel.send('There is no song that I could resume!');
-		if (!serverQueue.connection.dispatcher.paused) return message.channel.send('Song already resume!');
+		if (!serverQueue.connection.dispatcher.paused) return message.channel.send('Song already resumed!');
 		serverQueue.connection.dispatcher.resume();
 	},
 };
