eedb493d88f7d4586a87b46d010e3a55828c97d2
Michele Caputo <michele@caputoweb.xyz>
2021-09-14 18:55:19 +0200

    followUp instead of reply
    followUp instead of reply

--- commands/vincelfo.js
+++ commands/vincelfo.js
@@ -2,9 +2,8 @@ module.exports = {
   name: 'vincelfo',
   description: 'Comando segreto.',
   execute(interaction, client) {
-    interaction.reply({
+    interaction.followUp({
       content: `https://www.youtube.com/channel/UCJR4YnCNG9rwdG686IL5d7A`,
-      ephemeral: true,
     });
   },
 };
