4b6239dc5faf05f6f2c6f691761cc31dc628d409
Michele Caputo <michele@caputoweb.xyz>
2021-09-14 19:06:11 +0200

    Update vincelfo.js

--- commands/vincelfo.js
+++ commands/vincelfo.js
@@ -2,8 +2,6 @@ module.exports = {
   name: 'vincelfo',
   description: 'Comando segreto.',
   async execute(interaction, player) {
-   return void interaction.followUp({
-     content: 'https://www.youtube.com/channel/UCJR4YnCNG9rwdG686IL5d7A',
-   });
+   return void interaction.followUp({content: 'No results were found!'});
   },
 };
