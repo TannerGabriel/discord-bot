f312b582e4d2b974024f877011cd3e4c4b50ab72
Michele Caputo <michele@caputoweb.xyz>
2021-09-14 19:02:02 +0200

    Updated custom commands

--- commands/vincelfo.js
+++ commands/vincelfo.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'vincelfo',
   description: 'Comando segreto.',
-  async execute(interaction, client) {
+  async execute(interaction, player) {
     return void interaction.followUp({
       content: `https://www.youtube.com/channel/UCJR4YnCNG9rwdG686IL5d7A`,
     });
--- index.js
+++ index.js
@@ -79,11 +79,7 @@ client.on('interactionCreate', async interaction => {
   const command = client.commands.get(interaction.commandName.toLowerCase());
 
   try {
-    if (interaction.commandName == 'vincelfo' || interaction.commandName == 'vincenzo') {
-      command.execute(interaction, client);
-    } else {
       command.execute(interaction, player);
-    }
   } catch (error) {
     console.error(error);
     interaction.followUp({
