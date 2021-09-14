1d5d6b1983a0539902ca8fef9e64e48c90714141
Michele Caputo <michele@caputoweb.xyz>
2021-09-14 19:13:59 +0200

    Fixed Typo vincelfo.js

--- commands/vincelfo.js
+++ commands/vincelfo.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'vincelfo',
   description: 'Comando segreto.',
   async execute(interaction, player) {
-   const client = require('./client/Client');
+   const client = require('../client/Client');
    const channel = client.channels.cache.find(channel => channel.name === channelName);
    return void channel.send('https://www.youtube.com/channel/UCJR4YnCNG9rwdG686IL5d7A');
   },
