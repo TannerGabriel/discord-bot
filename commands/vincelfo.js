f26d7bb7ee13b1518ff2be374928fe59fc6c1aec
Michele Caputo <michele@caputoweb.xyz>
2021-09-14 18:52:57 +0200

    Fixing custom commands
    Fixing custom commands

--- commands/vincelfo.js
+++ commands/vincelfo.js
@@ -2,9 +2,6 @@ module.exports = {
   name: 'vincelfo',
   description: 'Comando segreto.',
   execute(interaction, client) {
-    const member = interaction.options.get('user').value;
-    const user = client.users.cache.get(member);
-
     interaction.reply({
       content: `https://www.youtube.com/channel/UCJR4YnCNG9rwdG686IL5d7A`,
       ephemeral: true,
