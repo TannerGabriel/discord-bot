b6f4e7edfb451ebaf6b09bedc86abd2f3b614d18
Michele Caputo <michele@caputoweb.xyz>
2021-09-16 18:31:58 +0200

    Aggiornati comandi custom
    Aggiornati comandi custom

---
+++ commands/setactivity.js
@@ -0,0 +1,54 @@
+module.exports = {
+  name: 'setactivity',
+  description: 'Cambia stato attività del bot',
+  options: [
+    {
+      name: 'descrizione',
+      type: 3, // 'STRING' Type
+      description: 'La descrizione dello stato del bot',
+      required: true,
+    },
+    {
+      name: 'stato',
+      type: 3, // 'STRING' Type
+      description: '1: "Sta giocando", 2: "Sta ascoltando", 3: "Sta guardando", 4: "In competizione su"',
+      required: true,
+    },
+  ],
+  async execute(interaction, player, client) {
+   const descr = interaction.options.get('descrizione').value;
+   const stato = interaction.options.get('stato').value;
+   var type;
+   var long_stato;
+
+   switch(stato) {
+    case '1':
+     type = 'PLAYING';
+     long_stato = 'Sta giocando a ';
+     break;
+    case '2':
+     type = 'LISTENING';
+     long_stato = 'Sta ascoltando ';
+     break;
+    case '3':
+     type = 'WATCHING';
+     long_stato = 'Sta guardando ';
+     break;
+    case '4':
+     type = 'COMPETING';
+     long_stato = 'In competizione su ';
+     break;
+    default:
+     return void interaction.reply('**Stato non modificato!**');
+   }
+
+   try {
+    client.user.setActivity(descr, { type: type });
+    var response = 'Stato del bot modificato in: ' + long_stato + '**' +  descr + '**!';
+    return void interaction.reply(response);
+   }
+   catch {
+    return void interaction.reply('**Stato non modificato!**');
+   }
+  },
+};
--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,54 +1,46 @@
 module.exports = {
   name: 'setstatus',
-  description: 'Cambia stato del bot',
-  options: [
-    {
-      name: 'descrizione',
-      type: 3, // 'STRING' Type
-      description: 'La descrizione dello stato del bot',
-      required: true,
-    },
+  description: 'Cambia stato online del bot',
     {
       name: 'stato',
       type: 3, // 'STRING' Type
-      description: '1: "Sta giocando", 2: "Sta ascoltando", 3: "Sta guardando", 4: "In competizione su"',
+      description: '1: "Online", 2: "Assente", 3: "Invisibile", 4: "Non disturbare"',
       required: true,
     },
   ],
   async execute(interaction, player, client) {
-   const descr = interaction.options.get('descrizione').value;
    const stato = interaction.options.get('stato').value;
    var type;
    var long_stato;
 
    switch(stato) {
     case '1':
-     type = 'PLAYING';
-     long_stato = 'Sta giocando a ';
+     type = 'online';
+     long_stato = 'Online ';
      break;
     case '2':
-     type = 'LISTENING';
-     long_stato = 'Sta ascoltando ';
+     type = 'idle';
+     long_stato = 'Assente ';
      break;
     case '3':
-     type = 'WATCHING';
-     long_stato = 'Sta guardando ';
+     type = 'invisible';
+     long_stato = 'Invisibile ';
      break;
     case '4':
-     type = 'COMPETING';
-     long_stato = 'In competizione su ';
+     type = 'dnd';
+     long_stato = 'Non Disturbare ';
      break;
     default:
-     return void interaction.reply('**Stato non modificato!**');
+     return void interaction.reply('**Stato online non modificato!**');
    }
 
    try {
-    client.user.setActivity(descr, { type: type });
-    var response = 'Stato del bot modificato in: ' + long_stato + '**' +  descr + '**!';
+    client.user.setStatus(type);
+    var response = 'Stato online del bot modificato in: **' + long_stato + '**!';
     return void interaction.reply(response);
    }
    catch {
-    return void interaction.reply('**Stato non modificato!**');
+    return void interaction.reply('**Stato online non modificato!**');
    }
   },
 };
