48560635ac685ebef623809dd91aae277960d528
TannerGabriel <gabrieltanner.code@gmail.com>
2021-09-24 19:20:22 +0200

    Changed playing icon

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ player.on('connectionError', (queue, error) => {
 });
 
 player.on('trackStart', (queue, track) => {
-  queue.metadata.send(`🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+  queue.metadata.send(`▶ | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
 });
 
 player.on('trackAdd', (queue, track) => {
