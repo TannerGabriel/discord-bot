bf0c7967a019379a1a68a198ac2463da7675107c
T0jgli <kovalikadam00@gmail.com>
2021-09-25 15:20:24 +0200

    test

--- commands/play.js
+++ commands/play.js
@@ -60,9 +60,7 @@ module.exports = {
         });
       }
 
-      await interaction.followUp({
-        content: `⏱ | Loading your ${searchResult.playlist ? 'playlist' : 'track'}...`,
-      });
+      await interaction.followUp();
       searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
       if (!queue.playing) await queue.play();
     } catch (error) {
