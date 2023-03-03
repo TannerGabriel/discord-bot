ac4461c38f165a80745c3314a882956eeedfe381
smclean117 <90870055+smclean117@users.noreply.github.com>
2023-03-03 19:41:47 +0000

    Update index.js (#213)
    Fix for player autopausing after quitting after 30-60s.
    
    https://github.com/Androz2091/discord-player/issues/1630#issuecomment-1452877967

--- index.js
+++ index.js
@@ -22,6 +22,21 @@ console.log(client.commands);
 
 const player = new Player(client);
 
+player.on('connectionCreate', (queue) => {
+    queue.connection.voiceConnection.on('stateChange', (oldState, newState) => {
+      const oldNetworking = Reflect.get(oldState, 'networking');
+      const newNetworking = Reflect.get(newState, 'networking');
+
+      const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
+        const newUdp = Reflect.get(newNetworkState, 'udp');
+        clearInterval(newUdp?.keepAliveInterval);
+      }
+
+      oldNetworking?.off('stateChange', networkStateChangeHandler);
+      newNetworking?.on('stateChange', networkStateChangeHandler);
+    });
+});
+
 player.on('error', (queue, error) => {
   console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
 });
