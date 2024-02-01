3b131b368ef69314ba856daf8d157b759c29a017
eliuttth-dev <eliuttth@gmail.com>
2024-02-01 17:36:59 -0300

    Enhanced handling of queued spam messages

---
+++ src/client/Client.js
@@ -0,0 +1,18 @@
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+
+module.exports = class extends Client {
+  constructor(config) {
+    super({
+      intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+      ],
+    });
+
+    this.commands = new Collection();
+
+    this.config = config;
+  }
+};
